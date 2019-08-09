0f84c08bb232d30a1c46f3a2bc075062281000c2
jtamayo <aaristizabal@transfiriendo.com>
2019-08-09 16:59:31 -0500

    Subir AdminPro

---
+++ adminpro/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+//Rutas
+import { APP_ROUTER } from './app.routes';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    APP_ROUTER
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
